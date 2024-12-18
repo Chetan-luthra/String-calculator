d07a78038ccdf5e01231a4647480e8f4c5febc65
chetan-luthra <chetanluthra9998@gmail.com>
2024-12-18 18:55:43 +0530

    added the other test cases

--- test/StringCalculator.test.ts
+++ test/StringCalculator.test.ts
@@ -8,13 +8,7 @@ describe('StringCalculator', () => {
         calculator = new StringCalculator();
     })
 
-    it('should return 0 for an empty string', () => {
-        expect(calculator.calculate('')).to.equal(0);
-    })
 
-    it('should return the number for a single number input', () => {
-        expect(calculator.calculate('1')).to.equal(1);
-    });
 
 
 })
